c5e7582266bf5c669ad5decab6c43ede536f4480
MJ <s35972rjpq@gmail.com>
2020-05-06 11:26:38 +0800

    Align coding style

--- client/main_client.cpp
+++ client/main_client.cpp
@@ -1,5 +1,5 @@
 /**
- ** Date: 2020/05/05
+ ** Date: 2020/05/06
  ** Author: Ming-Jun Jiang.
  ** IDE: Win10(1909) & Visual Studio 2015 (MSVC14).
  ** Description: create a client to sned/receive data to/from a server.
@@ -16,147 +16,125 @@
 class client
 {
 public:
-	client() :bufSize(0)
-	{
+	client() :kBufSize(0) {
 		// initialization of member variables.
-		iResult = 0;
-		clntSock = INVALID_SOCKET;
-		sendBuf = nullptr;
-		recvBuf = nullptr;
+		result_ = 0;
+		clnt_sock_ = INVALID_SOCKET;
+		send_buf_ = nullptr;
+		recv_buf_ = nullptr;
 	}
-	explicit client(const std::wstring &ip, const std::uint16_t &port, const int &_bufSize) :bufSize(_bufSize)
-	{ 
+	explicit client(const std::wstring &ip, const std::uint16_t &port, const int &bufSize) :kBufSize(bufSize) { 
 		// initialization of member variables.
-		iResult = 0;
-		clntSock = INVALID_SOCKET;
-		sendBuf = new char[bufSize] {0};
-		recvBuf = new char[bufSize] {0};
+		result_ = 0;
+		clnt_sock_ = INVALID_SOCKET;
+		send_buf_ = new char[kBufSize]();
+		recv_buf_ = new char[kBufSize]();
 
-		WSADATA wsaData;//Windows Sockets API (WSA) initialization.
-		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);// Winsock Version 2.2
-		if (iResult == NO_ERROR)
-		{
+		WSADATA wsa_data;//Windows Sockets API (WSA) initialization.
+		result_ = WSAStartup(MAKEWORD(2, 2), &wsa_data);// Winsock Version 2.2
+		if (result_ == NO_ERROR) {
 			std::cout << "Windows Sockets API initialization succeed.\n";
-			clntSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);// create a socket with IPv4 and TCP setting.
-			if (clntSock != INVALID_SOCKET)
-			{
-				std::cout << "client socket: " << clntSock << "\n";
-				sockaddr_in servAddr;
-				memset(&servAddr, 0, sizeof(servAddr));
-				servAddr.sin_family = AF_INET;// IPv4
-				InetPton(AF_INET, ip.c_str(), &servAddr.sin_addr.s_addr);// IP
-				servAddr.sin_port = htons(port);// Port
-				iResult = connect(clntSock, reinterpret_cast<SOCKADDR*>(&servAddr), sizeof(SOCKADDR));// connect a socket to the IP and port.
-				if (iResult == SOCKET_ERROR)
-				{
+			clnt_sock_ = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);// create a socket with IPv4 and TCP setting.
+			if (clnt_sock_ != INVALID_SOCKET) {
+				std::cout << "client socket: " << clnt_sock_ << "\n";
+				sockaddr_in serv_addr;
+				memset(&serv_addr, 0, sizeof(serv_addr));
+				serv_addr.sin_family = AF_INET;// IPv4
+				InetPton(AF_INET, ip.c_str(), &serv_addr.sin_addr.s_addr);// IP
+				serv_addr.sin_port = htons(port);// Port
+				result_ = connect(clnt_sock_, reinterpret_cast<SOCKADDR*>(&serv_addr), sizeof(SOCKADDR));// connect a socket to the IP and port.
+				if (result_ == SOCKET_ERROR) {
 					std::cerr << "Connection failed with error: " << WSAGetLastError() << "\n";
-					clntSock = INVALID_SOCKET;
+					clnt_sock_ = INVALID_SOCKET;
 				}
 			}
-			else
-			{
+			else {
 				std::cerr << "Invalid Socket with error:" << WSAGetLastError() << "\n";
 			}
 		}
-		else
-		{
+		else {
 			std::cerr << "Windows Sockets API initialization failed with error:" << WSAGetLastError() << "\n";
 		}
 	}
 	client(const client&) = delete; // non construction-copyable.
 	client& operator=(const client&) = delete; // non copyable.
-	~client()
-	{
-		closesocket(clntSock);// close client socket.
+	~client() {
+		closesocket(clnt_sock_);// close client socket.
 		WSACleanup();//Clean and close Windows Sockets API (WSA).
 
 		// release memory of buffer.
-		if (sendBuf != nullptr)
-		{
-			delete[] sendBuf;
-			sendBuf = nullptr;
+		if (send_buf_ != nullptr) {
+			delete[] send_buf_;
+			send_buf_ = nullptr;
 		}
-		if (recvBuf != nullptr)
-		{
-			delete[] recvBuf;
-			recvBuf = nullptr;
+		if (recv_buf_ != nullptr) {
+			delete[] recv_buf_;
+			recv_buf_ = nullptr;
 		}
 	}
 
-	void startSend()
-	{
-		if (clntSock != INVALID_SOCKET)
-		{
-			std::string strBuffer = "";
+	void StartSend() {
+		if (clnt_sock_ != INVALID_SOCKET) {
+			std::string str_buf = "";
 			std::cout << "enter data (enter EXIT for leaving).\n";
-			while (true)
-			{
-				std::cin >> strBuffer;
-				if ((strBuffer == "exit") || (strBuffer == "EXIT"))
-				{
+			while (true) {
+				std::cin >> str_buf;
+				if ((str_buf == "exit") || (str_buf == "EXIT")) {
 					break;
 				}
-				else
-				{
-					strncpy_s(sendBuf, bufSize, strBuffer.c_str(), _TRUNCATE);// copy input data to buffer.
-					iResult = send(clntSock, sendBuf, bufSize, 0);// send buffer to the server.
-					if (iResult == SOCKET_ERROR)
-					{
+				else {
+					strncpy_s(send_buf_, kBufSize, str_buf.c_str(), _TRUNCATE);// copy input data to buffer.
+					result_ = send(clnt_sock_, send_buf_, kBufSize, 0);// send buffer to the server.
+					if (result_ == SOCKET_ERROR) {
 						std::cerr << "Sending failed with error: " << WSAGetLastError() << "\n";
 						break;
 					}
 				}
 			}
-			shutdown(clntSock, 2);//shutdown the socket.
+			shutdown(clnt_sock_, 2);//shutdown the socket.
 		}
 	}
-	void startRecv()
-	{
-		if (clntSock != INVALID_SOCKET)
-		{
-			while (true)
-			{
-				iResult = recv(clntSock, recvBuf, bufSize, 0);// receive data from the server.
-				if (iResult > 0)
-				{
-					std::cout <<"Receive: "<< recvBuf << "\n";
+	void StartRecv() {
+		if (clnt_sock_ != INVALID_SOCKET) {
+			while (true) {
+				result_ = recv(clnt_sock_, recv_buf_, kBufSize, 0);// receive data from the server.
+				if (result_ > 0) {
+					std::cout <<"Receive: "<< recv_buf_ << "\n";
 				}
-				else if (iResult == 0)
-				{
+				else if (result_ == 0) {
 					std::cout << "connection is closed.\n";
 					break;
 				}
-				else
-				{
+				else {
 					std::cerr << "receiving failed with error: " << WSAGetLastError() << "\n";
 					break;
 				}
 			}
-			shutdown(clntSock, 2);//shutdown the socket.
+			shutdown(clnt_sock_, 2);//shutdown the socket.
 		}
 	}
 
 private:
-	SOCKET clntSock;
-	char *sendBuf;
-	char *recvBuf;
-	const int bufSize;
-	int iResult;
+	SOCKET clnt_sock_;
+	char *send_buf_;
+	char *recv_buf_;
+	const int kBufSize;
+	int result_;
 };
 
 int main()
 {
 	// Basic setting.
-	std::wstring IP = L"127.0.0.1";// IP.
+	std::wstring ip = L"127.0.0.1";// IP.
 	uint16_t port = 6666;// Port.
-	const int data_length = 512;// maximum data length.
+	const int kDataLength = 512;// maximum data length.
 
 	// Create a client.
-	client client1(IP, port, data_length);
-	std::thread recvThread([&client1]() {client1.startRecv(); });// creat a thread to receive data.
-	std::thread sendThread([&client1]() {client1.startSend(); });// creat a thread to send data.
-	recvThread.join();// wait for the receiving thread to finish.
-	sendThread.join();// wait for the sending thread to finish.
+	client client1(ip, port, kDataLength);
+	std::thread recv_thread([&client1]() {client1.StartRecv(); });// creat a thread to receive data.
+	std::thread send_thread([&client1]() {client1.StartSend(); });// creat a thread to send data.
+	recv_thread.join();// wait for the receiving thread to finish.
+	send_thread.join();// wait for the sending thread to finish.
 
 	std::cout << "Press enter to exit.\n";
 	getchar();
--- server/main_server.cpp
+++ server/main_server.cpp
@@ -1,5 +1,5 @@
 /**
- ** Date: 2020/05/05
+ ** Date: 2020/05/06
  ** Author: Ming-Jun Jiang.
  ** IDE: Win10(1909) & Visual Studio 2015 (MSVC14).
  ** Description: create a server to receive data from multiple clients and send data to all clients.
@@ -16,222 +16,183 @@
 class server
 {
 public:
-	server() :bufSize(0)
-	{
+	server() :kBufSize(0) {
 		// initialization of member variables.
-		servSock = INVALID_SOCKET;
-		maxSock = INVALID_SOCKET;
-		FD_ZERO(&master_fd);//initialize the master set.
-		sendBuf = nullptr;
-		recvBuf = nullptr;
-		sTimeout = 0;
-		usTimeout = 0;
-		iResult = 0;
-		isWorking = false;
+		serv_sock_ = INVALID_SOCKET;
+		max_sock_ = INVALID_SOCKET;
+		FD_ZERO(&master_fd_);//initialize the master set.
+		send_buf_ = nullptr;
+		recv_buf_ = nullptr;
+		timeout_s_ = 0;
+		timeout_us_ = 0;
+		result_ = 0;
+		is_working_ = false;
 	}
-	explicit server(const std::wstring &ip, const std::uint16_t &port, const int &_bufSize, const int& _sTimeout, const int& _usTimeout) :bufSize(_bufSize)
-	{
+	explicit server(const std::wstring &ip, const std::uint16_t &port, const int &buf_size, const int& timeout_s, const int& timeout_us) :kBufSize(buf_size) {
 		// initialization of member variables
-		servSock = INVALID_SOCKET;
-		maxSock = INVALID_SOCKET;
-		FD_ZERO(&master_fd);//initialize the master set.
-		sendBuf = new char[bufSize] {0};
-		recvBuf = new char[bufSize] {0};
-		sTimeout = _sTimeout;
-		usTimeout = _usTimeout;
-		iResult = 0;
-		isWorking = false;
+		serv_sock_ = INVALID_SOCKET;
+		max_sock_ = INVALID_SOCKET;
+		FD_ZERO(&master_fd_);//initialize the master set.
+		send_buf_ = new char[kBufSize]();
+		recv_buf_ = new char[kBufSize]();
+		timeout_s_ = timeout_s;
+		timeout_us_ = timeout_us;
+		result_ = 0;
+		is_working_ = false;
 
-		WSADATA wsaData;//Windows Sockets API (WSA) initialization.
-		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);// Winsock Version 2.2
-		if (iResult == NO_ERROR)
-		{
+		WSADATA wsa_data;//Windows Sockets API (WSA) initialization.
+		result_ = WSAStartup(MAKEWORD(2, 2), &wsa_data);// Winsock Version 2.2
+		if (result_ == NO_ERROR) {
 			std::cout << "Windows Sockets API initialization succeed.\n";
-			servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);// create a socket with IPv4 and TCP setting.
-			if (servSock != INVALID_SOCKET)
-			{
-				std::cout << "server socket: " << servSock << "\n";
-				sockaddr_in servAddr;
-				memset(&servAddr, 0, sizeof(servAddr));
-				servAddr.sin_family = AF_INET;// IPv4
-				InetPton(AF_INET, ip.c_str(), &servAddr.sin_addr.s_addr);// IP
-				servAddr.sin_port = htons(port);// Port
-				iResult = bind(servSock, reinterpret_cast<SOCKADDR*>(&servAddr), sizeof(SOCKADDR));// bind a socket to the IP and port.
-				if (iResult != SOCKET_ERROR)
-				{
-					iResult = listen(servSock, 30);//set to listening mode and specify maximum of 30 pending connections for the server socket.
-					if (iResult == SOCKET_ERROR)
-					{
+			serv_sock_ = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);// create a socket with IPv4 and TCP setting.
+			if (serv_sock_ != INVALID_SOCKET) {
+				std::cout << "server socket: " << serv_sock_ << "\n";
+				sockaddr_in serv_addr;
+				memset(&serv_addr, 0, sizeof(serv_addr));
+				serv_addr.sin_family = AF_INET;// IPv4
+				InetPton(AF_INET, ip.c_str(), &serv_addr.sin_addr.s_addr);// IP
+				serv_addr.sin_port = htons(port);// Port
+				result_ = bind(serv_sock_, reinterpret_cast<SOCKADDR*>(&serv_addr), sizeof(SOCKADDR));// bind a socket to the IP and port.
+				if (result_ != SOCKET_ERROR) {
+					result_ = listen(serv_sock_, 30);//set to listening mode and specify maximum of 30 pending connections for the server socket.
+					if (result_ == SOCKET_ERROR) {
 						std::cerr << "Listening failed with error: " << WSAGetLastError() << "\n";
-						servSock = INVALID_SOCKET;
+						serv_sock_ = INVALID_SOCKET;
 					}
 				}
-				else
-				{
-				    std::cerr << "Connection failed with error: " << WSAGetLastError() << "\n";
-					servSock = INVALID_SOCKET;
+				else {
+					std::cerr << "Connection failed with error: " << WSAGetLastError() << "\n";
+					serv_sock_ = INVALID_SOCKET;
 				}
 			}
-			else
-			{
+			else {
 				std::cerr << "Invalid Socket with error:" << WSAGetLastError() << "\n";
 			}
 		}
-		else
-		{
+		else {
 			std::cerr << "Windows Sockets API initialization failed with error:" << WSAGetLastError() << "\n";
 		}
 	}
 	server(const server&) = delete; // non construction-copyable.
 	server& operator=(const server&) = delete; // non copyable.
-	~server()
-	{
-		closesocket(servSock);// close server socket.
+	~server() {
+		closesocket(serv_sock_);// close server socket.
 		WSACleanup();//Clean and close Windows Sockets API (WSA).
 
 		// release memory of buffer.
-		if (sendBuf != nullptr)
+		if (send_buf_ != nullptr)
 		{
-			delete[] sendBuf;
-			sendBuf = nullptr;
+			delete[] send_buf_;
+			send_buf_ = nullptr;
 		}
-		if (recvBuf != nullptr)
+		if (recv_buf_ != nullptr)
 		{
-			delete[] recvBuf;
-			recvBuf = nullptr;
+			delete[] recv_buf_;
+			recv_buf_ = nullptr;
 		}
 	}
 
-	void startRecv()
-	{
-		if (servSock != INVALID_SOCKET)
-		{
+	void StartRecv() {
+		if (serv_sock_ != INVALID_SOCKET) {
 			// initialization of FD
-			timeval timeout = { sTimeout,usTimeout };// set timeout to 10 seconds.
+			timeval timeout = { timeout_s_, timeout_us_ };// set timeout to 10 seconds.
 			fd_set read_fd;//sets of file descriptors.
 			FD_ZERO(&read_fd);//initialize the reading set.
-			FD_SET(servSock, &master_fd);//add server socket to master set
+			FD_SET(serv_sock_, &master_fd_);//add server socket to master set
 
-			maxSock = servSock;// record the maximum socket.
-			SOCKET clntSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-			sockaddr_in clntAddr;
-			int nSize = sizeof(SOCKADDR);
+			max_sock_ = serv_sock_;// record the maximum socket.
+			SOCKET clnt_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+			sockaddr_in clnt_addr;
+			int addr_size = sizeof(SOCKADDR);
 
-			isWorking = true;
-			while (isWorking)
-			{
-				read_fd = master_fd;//refresh reading set with master set;
-				iResult = select(static_cast<int>(maxSock + 1), &read_fd, NULL, NULL, &timeout);//polling all sockets in the reading set.
-				if (iResult > 0)
-				{
-					for (unsigned int i = 0; i <= maxSock; i++)
-					{
+			is_working_ = true;
+			while (IsWorking()) {
+				read_fd = master_fd_;//refresh reading set with master set;
+				result_ = select(static_cast<int>(max_sock_ + 1), &read_fd, NULL, NULL, &timeout);//polling all sockets in the reading set.
+				if (result_ > 0) {
+					for (unsigned int i = 0; i <= max_sock_; ++i) {
 						// check sockets in the reading set.
-						if (FD_ISSET(i, &read_fd))
-						{
-							if (i == servSock)
-							{
+						if (FD_ISSET(i, &read_fd)) {
+							if (i == serv_sock_) {
 								std::cout << "Connecting a new client";
-								clntSock = accept(servSock, reinterpret_cast<SOCKADDR*>(&clntAddr), &nSize);// accept the connection from a client.
-								if (clntSock != INVALID_SOCKET)
-								{
-									std::cout << " with the socket: " << clntSock << "\n";
-									FD_SET(clntSock, &master_fd);// add a client socket to the master set.
-									if (clntSock > maxSock)
-									{
-										maxSock = clntSock;// update the maximum socket.
+								clnt_sock = accept(serv_sock_, reinterpret_cast<SOCKADDR*>(&clnt_addr), &addr_size);// accept the connection from a client.
+								if (clnt_sock != INVALID_SOCKET) {
+									std::cout << " with the socket: " << clnt_sock << "\n";
+									FD_SET(clnt_sock, &master_fd_);// add a client socket to the master set.
+									if (clnt_sock > max_sock_) {
+										max_sock_ = clnt_sock;// update the maximum socket.
 									}
 
-									strncpy_s(sendBuf, bufSize, "Welcome to your server!", _TRUNCATE);//
-									iResult = send(clntSock, sendBuf, bufSize, 0);// send buffer to a client.
-									if (iResult == SOCKET_ERROR)
-									{
+									strncpy_s(send_buf_, kBufSize, "Welcome to your server!", _TRUNCATE);//
+									result_ = send(clnt_sock, send_buf_, kBufSize, 0);// send buffer to a client.
+									if (result_ == SOCKET_ERROR) {
 										std::cerr << "Send failed with error: " << WSAGetLastError() << "\n";
-										FD_CLR(clntSock, &master_fd);// remove the client socket from the master set.
-										closesocket(clntSock); // close the client socket.
+										FD_CLR(clnt_sock, &master_fd_);// remove the client socket from the master set.
+										closesocket(clnt_sock); // close the client socket.
 									}
 								}
-								else
-								{
+								else {
 									std::cerr << ".\nAccepting failed with error: " << WSAGetLastError() << "\n";
 								}
 							}
-							else
-							{
-								iResult = recv(i, recvBuf, bufSize, 0);//receive data from a client.
-								if (iResult > 0)
-								{
-									std::cout << "Client " << i << ":" << recvBuf << "\n";
+							else {
+								result_ = recv(i, recv_buf_, kBufSize, 0);//receive data from a client.
+								if (result_ > 0) {
+									std::cout << "Client " << i << ":" << recv_buf_ << "\n";
 								}
-								else if (iResult == 0)
-								{
+								else if (result_ == 0) {
 									std::cout << "Client " << i << ": connection is closed.\n";
-									FD_CLR(i, &master_fd);// remove the client socket from the master set.
+									FD_CLR(i, &master_fd_);// remove the client socket from the master set.
 									closesocket(i); // close the client socket.
 								}
-								else
-								{
+								else {
 									std::cerr << "Client " << i << ":receiving failed with error: " << WSAGetLastError() << "\n";
-									FD_CLR(i, &master_fd);// remove the client socket from the master set.
+									FD_CLR(i, &master_fd_);// remove the client socket from the master set.
 									closesocket(i); // close the client socket.
 								}
 							}
 						}
 					}
 				}
-				else if (iResult == 0)
-				{
+				else if (result_ == 0) {
 					//std::cerr << "Waiting Time Out.\n";
 				}
-				else 
-				{
+				else {
 					std::cerr << "Waiting failed with error: " << WSAGetLastError() << "\n";
 					break;
 				}
 			}
 
 			// clean FD.
-			for (unsigned int i = 0; i <= maxSock; i++)
-			{
-				if (FD_ISSET(i, &master_fd))
-				{
-					FD_CLR(i, &master_fd);// remove the client socket from the master set.
+			for (unsigned int i = 0; i <= max_sock_; ++i) {
+				if (FD_ISSET(i, &master_fd_)) {
+					FD_CLR(i, &master_fd_);// remove the client socket from the master set.
 					closesocket(i); // close the client socket.
 				}
 			}
 		}
 	}
-	void startSend()
-	{
-		if (servSock != INVALID_SOCKET)
-		{
-			std::string strBuffer = "";
+	void StartSend() {
+		if (serv_sock_ != INVALID_SOCKET) {
+			std::string str_buf = "";
 			std::cout << "enter data (enter EXIT for leaving).\n";
-			while (true)
-			{
-				std::cin >> strBuffer;
-				if ((strBuffer == "exit") || (strBuffer == "EXIT"))
-				{
-					for (unsigned int i = 0; i <= maxSock; i++)
-					{
-						if (FD_ISSET(i, &master_fd))
-						{
+			while (true) {
+				std::cin >> str_buf;
+				if ((str_buf == "exit") || (str_buf == "EXIT")) {
+					for (unsigned int i = 0; i <= max_sock_; ++i) {
+						if (FD_ISSET(i, &master_fd_)) {
 							shutdown(i, 2);// shutdown all sockets in the master fd set.
 						}
 					}
-					isWorking = false;// to stop the receiving loop.
+					is_working_ = false;// to stop the receiving loop.
 					break;
 				}
-				else
-				{
-					for (unsigned int i = 0; i <= maxSock; i++)
-					{
-						if ((i != servSock) && FD_ISSET(i, &master_fd))
-						{
-							strncpy_s(sendBuf, bufSize, strBuffer.c_str(), _TRUNCATE);// copy input data to buffer.
-							iResult = send(i, sendBuf, bufSize, 0);// send buffer to a client.
-							if (iResult == SOCKET_ERROR)
-							{
+				else {
+					for (unsigned int i = 0; i <= max_sock_; ++i) {
+						if ((i != serv_sock_) && FD_ISSET(i, &master_fd_)) {
+							strncpy_s(send_buf_, kBufSize, str_buf.c_str(), _TRUNCATE);// copy input data to buffer.
+							result_ = send(i, send_buf_, kBufSize, 0);// send buffer to a client.
+							if (result_ == SOCKET_ERROR) {
 								std::cerr << "Send failed with error: " << WSAGetLastError() << "\n";
 								shutdown(i, 2);//shutdown a socket.
 							}
@@ -241,35 +202,35 @@ public:
 			}
 		}
 	}
+	bool IsWorking() const { return is_working_; }
 
 private:
-	SOCKET servSock;
-	SOCKET maxSock;
-	fd_set master_fd;
-	char *sendBuf;
-	char *recvBuf;
-	const int bufSize;
-	int iResult;
-	int sTimeout;
-	int usTimeout;
-	volatile bool isWorking;
+	SOCKET serv_sock_;
+	SOCKET max_sock_;
+	fd_set master_fd_;
+	char *send_buf_;
+	char *recv_buf_;
+	const int kBufSize;
+	int result_;
+	int timeout_s_;
+	int timeout_us_;
+	volatile bool is_working_;
 };
 
-int main()
-{
-	// Basic setting.
-	std::wstring IP = L"127.0.0.1";// IP.
+int main() {
+	// Basic settings.
+	std::wstring ip = L"127.0.0.1";// IP.
 	uint16_t port = 6666;// Port.
-	const int data_length = 512;// maximum data length.
-	int timeout_second = 1;// timeout setting in seconds.
-	int timeout_usecond = 0;// timeout setting in microseconds.
+	const int kDataLength = 512;// maximum data length.
+	int timeout_s = 1;// timeout setting in seconds.
+	int timeout_us = 0;// timeout setting in microseconds.
 
-	// Create a server.
-	server server1(IP, port, data_length, timeout_second, timeout_usecond);
-	std::thread recvThread([&server1]() {server1.startRecv(); });// creat a thread to receive data.
-	std::thread sendThread([&server1]() {server1.startSend(); });// creat a thread to send data.
-	recvThread.join();// wait for the receiving thread to finish.
-	sendThread.join();// wait for the sending thread to finish.
+
+	server server1(ip, port, kDataLength, timeout_s, timeout_us);// Create a server.
+	std::thread recv_thread([&server1]() {server1.StartRecv(); });// creat a thread to receive data.
+	std::thread send_thread([&server1]() {server1.StartSend(); });// creat a thread to send data.
+	recv_thread.join();// wait for the receiving thread to finish.
+	send_thread.join();// wait for the sending thread to finish.
 
 	std::cout << "Press enter to exit.\n";
 	getchar();
